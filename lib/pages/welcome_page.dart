0834c8eb6bc447618416bb14cde8ce2f59ce3dfb
Shakil Ahmed <sakilhossain01969@gmail.com>
2024-01-20 01:09:23 +0600

    working: welcome page

--- lib/pages/welcome_page.dart
+++ lib/pages/welcome_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:travel_app/misc/colors.dart';
+import 'package:travel_app/widgets/app_large_text.dart';
+import 'package:travel_app/widgets/app_text.dart';
 
 class WelcomePage extends StatefulWidget {
   const WelcomePage({super.key});
@@ -8,20 +11,47 @@ class WelcomePage extends StatefulWidget {
 }
 
 class _WelcomePageState extends State<WelcomePage> {
+  List images = [
+    "welcome-one.png",
+    "welcome-two.png",
+    "welcome-three.png"
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: PageView.builder(
         scrollDirection: Axis.vertical,
-        itemCount: 3,
+        itemCount: images.length,
         itemBuilder: (_, index) {
           return Container(
             width: double.maxFinite,
             height: double.maxFinite,
             decoration: BoxDecoration(
               image: DecorationImage(
-                
+                image: AssetImage('img/'+images[index]),
+                fit: BoxFit.cover
               )
+            ),
+            child: Container(
+              margin: const EdgeInsets.only(top: 150, left: 20, right: 20),
+              child: Row(children: [
+                Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    AppLargeText(text: 'Trips '),
+                    AppText(text: 'Mountain', size: 30),
+                    const SizedBox(height: 20),
+                    Container(
+                      width: 250,
+                      child: AppText(text: "Mountain hikes give you an incredible sense of freedom along with endurance tests",
+                        color: AppColors.textColor2,
+                        size: 14,
+                      ),
+                    )
+                  ],
+                )
+              ]),
             ),
           );
       }),
