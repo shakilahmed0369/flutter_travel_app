db9a19b184304530cff2698f706e77e4a78d2741
Shakil Ahmed <sakilhossain01969@gmail.com>
2024-01-24 19:55:11 +0600

    wokring: home page

--- lib/pages/nav_pages/home_page.dart
+++ lib/pages/nav_pages/home_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:travel_app/misc/colors.dart';
+import 'package:travel_app/widgets/app_large_text.dart';
+import 'package:travel_app/widgets/app_text.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -7,9 +10,174 @@ class HomePage extends StatefulWidget {
   State<HomePage> createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage> {
+class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    TabController _tabController = TabController(length: 3, vsync: this);
+
+    return Scaffold(
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            padding: const EdgeInsets.only(top: 70, left: 20),
+            child: Row(
+              children: [
+                Icon(
+                  Icons.menu,
+                  size: 30,
+                  color: Colors.black54,
+                ),
+                Expanded(child: Container()),
+                Container(
+                  margin: const EdgeInsets.only(right: 20),
+                  width: 50,
+                  height: 50,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: Colors.grey.withOpacity(0.5)),
+                )
+              ],
+            ),
+          ),
+          const SizedBox(height: 40),
+          Container(
+            margin: const EdgeInsets.only(left: 20),
+            child: AppLargeText(text: 'Discover'),
+          ),
+          const SizedBox(height: 40),
+          Container(
+            child: TabBar(
+              tabAlignment: TabAlignment.start,
+              labelPadding: const EdgeInsets.only(left: 20, right: 20),
+              controller: _tabController,
+              labelColor: Colors.black,
+              unselectedLabelColor: Colors.grey,
+              dividerHeight: 0.0,
+              isScrollable: true,
+              indicator:
+                  CircleTabIndigator(color: AppColors.mainColor, radius: 4),
+              tabs: [
+                Tab(
+                  text: 'Places',
+                ),
+                Tab(
+                  text: 'Insparation',
+                ),
+                Tab(
+                  text: 'Emotions',
+                ),
+              ],
+            ),
+          ),
+          Container(
+            padding: const EdgeInsets.only(left: 20),
+            height: 300,
+            width: double.maxFinite,
+            child: TabBarView(
+              controller: _tabController,
+              children: [
+                ListView.builder(
+                  itemCount: 3,
+                  scrollDirection: Axis.horizontal,
+                  itemBuilder: (BuildContext context, int index) {
+                    return Container(
+                    margin: const EdgeInsets.only(right: 15, top: 10),
+                      width: 200,
+                      height: 300,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                        color: Colors.white,
+                        image: const DecorationImage(
+                            image: AssetImage('img/mountain.jpeg'),
+                            fit: BoxFit.cover),
+                      ),
+                    );
+                  },
+                ),
+                Text('hi'),
+                Text('by'),
+              ],
+            ),
+          ),
+          const SizedBox(height: 30),
+          Container(
+            margin: const EdgeInsets.only(left: 20, right: 20),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                AppLargeText(text: 'Explore more', size: 22,),
+                AppText(text: 'See all', color: AppColors.textColor1,)
+              ],
+            ),
+          ),
+          const SizedBox(height: 20),
+          Container(
+            height: 120,
+            width: double.maxFinite,
+            margin: const EdgeInsets.only(left: 20),
+            child: ListView.builder(
+              scrollDirection: Axis.horizontal,
+              itemCount: 4,
+              itemBuilder: (_, index) {
+                return Container(
+                  margin: const EdgeInsets.only(right: 50),
+                  child: Column(
+                    children: [
+                      Container(
+                        width: 80,
+                        height: 80,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(20),
+                          color: Colors.white,
+                          image: const DecorationImage(
+                              image: AssetImage('img/mountain.jpeg'),
+                              fit: BoxFit.cover),
+                        ),
+                      ),
+                      Container(
+                        child: AppText(text: 'Kayking'),
+                      )
+                    ],
+                  ),
+                );
+              }
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class CircleTabIndigator extends Decoration {
+  final Color color;
+  final double radius;
+
+  const CircleTabIndigator({required this.color, required this.radius});
+
+  @override
+  BoxPainter createBoxPainter([VoidCallback? onChanged]) {
+    return _CirclePointer(color: color, radius: radius);
+  }
+}
+
+class _CirclePointer extends BoxPainter {
+  final Color color;
+  double radius;
+
+  _CirclePointer({required this.color, required this.radius});
+
+  @override
+  void paint(Canvas canvas, Offset offset, ImageConfiguration configuration) {
+    Paint _paint = Paint();
+    _paint.color = color;
+    _paint.isAntiAlias = true;
+
+    final Offset circleOffset = Offset(
+        configuration.size!.width / 2 - radius / 2,
+        configuration.size!.height - radius);
+
+    canvas.drawCircle(offset + circleOffset, radius, _paint);
   }
 }
